38b41c6ad79fc0076b219188b763cf5671c55732
kipmcl <107448071+kipmcl@users.noreply.github.com>
2022-08-04 20:13:09 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log(`Hello, ${userName}!`);
+console.log(userQuestion);
+console.log(randomNumber);
+
+if (randomNumber === 0) {
+    eightBall = 'It is certain';
+    console.log(eightBall);
+} else if (randomNumber === 1) {
+    eightBall = 'It is decidedly so';
+    console.log(eightBall);
+} else if (randomNumber === 2) {
+    eightBall = 'Reply hazy try again';
+    console.log(eightBall);
+} else if (randomNumber === 3) {
+    eightBall = 'Cannot predict now';
+    console.log(eightBall);
+} else if (randomNumber === 4) {
+    eightBall = 'Do not count on it';
+    console.log(eightBall);
+} else if (randomNumber === 5) {
+    eightBall = 'My sources say no';
+    console.log(eightBall);
+}  else if (randomNumber === 6) {
+    eightBall = 'Outlook not so good';
+    console.log(eightBall);
+} else if (randomNumber === 7) {
+    eightBall = 'Signs point to yes';
+    console.log(eightBall);
+}
